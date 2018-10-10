e8292ec0cf86d3d3d3a038f48918d6eb91bf612e
Brad Abrahams <email@bradabrahams.co.uk>
2018-10-10 12:17:08 +0100

    section -> div

--- src/js/containers/App/index.js
+++ src/js/containers/App/index.js
@@ -7,12 +7,12 @@ import ModuleTwo from "../../components/ModuleTwo";
 export default class App extends React.Component {
   render() {
     return (
-      <section>
+      <div>
         <GlobalStyle />
         <h1>Hello World!</h1>
         <ModuleOne />
         <ModuleTwo />
-      </section>
+      </div>
     );
   }
 }
