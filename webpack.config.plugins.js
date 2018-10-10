e5f7be014a1d05e9a21f5d0c86825af62603034a
Brad Abrahams <email@bradabrahams.co.uk>
2018-10-10 11:19:32 +0100

    Get rid of stylelint

--- webpack.config.plugins.js
+++ webpack.config.plugins.js
@@ -1,30 +1,28 @@
-const cssnano = require('cssnano');
-const path = require('path');
-const webpack = require('webpack');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const StyleLintPlugin = require('stylelint-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const cssnano = require("cssnano");
+const path = require("path");
+const webpack = require("webpack");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 
-const paths = require('./paths');
+const paths = require("./paths");
 
 module.exports = {
   dev: [
     new HtmlWebpackPlugin({
-      template: path.join(paths.src, 'index.html'),
+      template: path.join(paths.src, "index.html"),
     }),
     new webpack.NamedModulesPlugin(),
     new webpack.HotModuleReplacementPlugin(),
-    new StyleLintPlugin(),
   ],
   prod: [
     new CleanWebpackPlugin(paths.dist),
     new HtmlWebpackPlugin({
-      template: path.join(paths.src, 'index.html'),
+      template: path.join(paths.src, "index.html"),
     }),
-    new ExtractTextPlugin('app.bundle.css'),
+    new ExtractTextPlugin("app.bundle.css"),
     new UglifyJSPlugin({
       sourceMap: true,
     }),
